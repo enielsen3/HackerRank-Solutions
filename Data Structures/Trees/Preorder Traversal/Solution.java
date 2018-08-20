f43d6fd7275cabae246358b1c8528a5d46fa9eb2
enielsen3 <39856608+enielsen3@users.noreply.github.com>
2018-08-20 15:55:14 -0400

    Create Solution.java

---
+++ Data Structures/Trees/Preorder Traversal/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static void preOrder(Node root) {
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+
+    }
+}
